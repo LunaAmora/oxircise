c086c79d2100caee70c847e0a0fde72e768f3f1a
Luna Amora <luna.mberry@gmail.com>
2022-10-22 20:07:07 -0400

    Add assets and wasm configuration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 use bevy::prelude::*;
-
-fn hello_world() {
-    println!("hello world!");
-}
+use oxilib::log;
 
 fn main() {
+    // When building for WASM, print panics to the browser console
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
-        .add_system(hello_world)
+        .insert_resource(WindowDescriptor { fit_canvas_to_parent: true, ..default() })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
         .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(SpriteBundle {
+        texture: asset_server.load("emoji-dzuk/emoji/ferris_64.png"),
+        ..default()
+    });
+
+    log!("The setup is done!");
 }
